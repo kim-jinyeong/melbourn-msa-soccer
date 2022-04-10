1403815352f3dd301410032e34b3b724d45978af
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-04-10 23:49:17 +0900

    22-04-10

--- soccer-express/app/routes/user.route.js
+++ soccer-express/app/routes/user.route.js
@@ -2,4 +2,5 @@ const { signup ,userlist } = require('../controllers/user.controller');
 module.exports = x => {
     x.app.post(`${x.url}/signup`, signup) 
     x.app.get(`${x.url}/list`,userlist)
+    x.app.get(`${x.url}/profile/:id`, profile)
 }
